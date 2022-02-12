3cc6f32cc0556353c6e47500d91778f5ba92431b
Khoroshavin <khoroshavin.russia@gmail.com>
2022-02-12 20:42:43 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// импорт компонента App
+import App from './App'
+
+// создание и монтирование приложения в #app
+createApp(App).mount('#app')
