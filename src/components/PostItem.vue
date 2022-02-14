010c0ee76413b5f58904ff74abc8acaacc772c9f
Khoroshavin <khoroshavin.russia@gmail.com>
2022-02-14 19:34:44 +0100

    add vue router dynamic navigation

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -7,10 +7,17 @@
     </div>
     <div class="post__btns">
       <my-button
+        @click="$router.push(`/posts/${post.id}`)"
+        class="btn-mr"
+      >
+      Open
+      </my-button>
+      <my-button
         @click="$emit('remove', post)"
       >
       Remove
       </my-button>
+      
     </div>
   </div>
 </template>
@@ -36,5 +43,9 @@ export default {
   justify-content: space-between;
   border: 2px solid #41B883;
   border-radius: 15px;
+}
+.post__btns {
+  display: flex;
+  margin-left: 20px;
 }
 </style>
